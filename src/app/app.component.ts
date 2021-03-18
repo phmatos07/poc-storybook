f5e7ffd00d8d71c3e2bfce1b30ad0d761451c3c8
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-18 18:15:02 -0300

    Implementação do componente 'RegisterUserComponent'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poc-storybook';
+  title = 'POC-Storybook - Formulário de Cadastro';
 }
