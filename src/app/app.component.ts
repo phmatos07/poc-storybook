57c00f1a44a2fbf4b8708314f6e2480347fdd980
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-19 14:53:59 -0300

    Implementação do 'Output' e criação do método 'dataChanges'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegisterUserInterface } from './model/register-user.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'POC-Storybook - Formulário de Cadastro';
+
+  dataChanges(values: RegisterUserInterface): void {
+    console.log(values);
+  }
 }
