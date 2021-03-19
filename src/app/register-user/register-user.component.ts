ce7ffba61aa9d09079a8baaf6102ded8a9b6530e
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-19 14:50:19 -0300

    Implementação completa dos métodos e funções referente ao FormBuilder

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { RegisterUserInterface } from './../model/register-user.interface';
+import { ALERT_MESSAGES_CONST } from './model/alert-messages.const';
+import { AlertMessagesInterface } from './model/alert-messages.model';
+import { ValidationTypeEnum } from './model/message-type.enum';
 
 @Component({
   selector: 'app-register-user',
@@ -7,6 +11,9 @@ import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register-user.component.scss']
 })
 export class RegisterUserComponent implements OnInit {
+
+  @Output()
+  dataChanges = new EventEmitter<RegisterUserInterface>();
 
   registerUserForm: AbstractControl | null = null;
 
@@ -18,15 +25,56 @@ export class RegisterUserComponent implements OnInit {
     this.registerUser();
   }
 
+  getAlert(inputName: string, control: AbstractControl): string {
+    const ALERT_MESSAGE = ALERT_MESSAGES_CONST.find((data: AlertMessagesInterface) => (
+      data.inputName === inputName &&
+      control.touched &&
+      control.errors &&
+      control.errors.hasOwnProperty(ValidationTypeEnum.REQUIRED)
+    )
+    );
+    return ALERT_MESSAGE?.message || '';
+  }
+
   onSubmit(): void {
-    console.log(this.registerUserForm);
+    try {
+      if (this.registerUserForm?.valid) {
+        this.dataChanges.emit({
+          nome: this.nome.value,
+          email: this.email.value,
+          password: this.password.value
+        });
+      }
+      throw new Error('Preencha o formulário para receber todos os dados!');
+    } catch (error) {
+      console.group('FORMULÁRIO DE CADASTRO');
+      console.error(error)
+      console.log({
+        nome: this.nome.value,
+        email: this.email.value,
+        password: this.password.value
+      });
+      console.groupEnd();
+    }
   }
 
   private registerUser(): void {
     this.registerUserForm = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.maxLength(50)]],
-      email: [null, Validators.required, Validators.maxLength(50)],
-      passwords: [null, Validators.required, Validators.maxLength(50)]
+      email: [null, [Validators.required, Validators.maxLength(50)]],
+      password: [null, [Validators.required, Validators.maxLength(50)]]
     });
+  }
+
+  get nome(): AbstractControl | null {
+    return this.registerUserForm?.get('nome') || null;
+  }
+
+  get email(): AbstractControl | null {
+    return this.registerUserForm?.get('email') || null;
+  }
+
+  get password(): AbstractControl | null {
+    return this.registerUserForm?.get('password') || null;
   }
 }
