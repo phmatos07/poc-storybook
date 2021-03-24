4d022d330a158859c1ab8d8756301042a750d9ad
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-23 21:10:25 -0300

    Instalação e configuração do 'addon-controls'

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-controls',
     '@storybook/addon-actions',
     '@storybook/addon-notes/register'
   ]
